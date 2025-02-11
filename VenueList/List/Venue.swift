51b8e53e0ea46fae5e62c0208d4c05d0b3ee5bfa
John Paul Manoza <johnpaul.manoza@ph.ncs-i.com>
2025-02-11 15:50:11 +0800

    fix(list): safely handle undefined values from the response

--- VenueList/List/Venue.swift
+++ VenueList/List/Venue.swift
@@ -27,16 +27,53 @@ struct Venue: Codable {
         case code, name, address, city, state, postcode, latitude, longitude, timezone
         case paxLocations = "pax_locations"
     }
+
+    // NOTE: Safely handle if one or many props has no value
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.code = try container.decodeIfPresent(String.self, forKey: .code) ?? ""
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        self.address = try container.decodeIfPresent(String.self, forKey: .address) ?? ""
+        self.city = try container.decodeIfPresent(String.self, forKey: .city) ?? ""
+        self.state = try container.decodeIfPresent(String.self, forKey: .state) ?? ""
+        self.postcode = try container.decodeIfPresent(String.self, forKey: .postcode) ?? ""
+        self.latitude = try container.decodeIfPresent(Double.self, forKey: .latitude) ?? 0.0
+        self.longitude = try container.decodeIfPresent(Double.self, forKey: .longitude) ?? 0.0
+        self.timezone = try container.decodeIfPresent(String.self, forKey: .timezone) ?? ""
+        self.paxLocations = try container.decodeIfPresent([PaxLocation].self, forKey: .paxLocations) ?? []
+    }
 }
 
 struct PaxLocation: Codable {
     let name: String
     let gates: [Gate]
+
+    enum CodingKeys: String, CodingKey {
+        case name, gates
+    }
+
+    // NOTE: Safely handle if one or many props has no value
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        self.gates = try container.decodeIfPresent([Gate].self, forKey: .gates) ?? []
+    }
 }
 
 struct Gate: Codable {
     let name: String
+
+    enum CodingKeys: String, CodingKey {
+        case name
+    }
+
+    // NOTE: Safely handle if one or many props has no value
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+    }
 }
+
 
 struct VenueCurrLocation: Identifiable, Equatable {
     var id = UUID()
