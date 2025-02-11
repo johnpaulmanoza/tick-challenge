014eec8b430b5a6256281293a29cc7823e453547
John Paul Manoza <johnpaul.manoza@ph.ncs-i.com>
2025-02-11 13:34:51 +0800

    Initial Commit

---
+++ VenueList/VenueListApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenueListApp.swift
+//  VenueList
+//
+//  Created by John Paul Manoza (NCS) on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VenueListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
