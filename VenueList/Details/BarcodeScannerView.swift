1c791584fa15defb251ff67414e4949f8d8976ec
John Paul Manoza <johnpaul.manoza@ph.ncs-i.com>
2025-02-11 16:14:49 +0800

    feat(details): added barcode scanner view

---
+++ VenueList/Details/BarcodeScannerView.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+import AVFoundation
+
+struct BarcodeScannerView: UIViewControllerRepresentable {
+    var onCaptureBarcode: (String) -> Void
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = UIViewController()
+        let captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return viewController }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return viewController
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return viewController
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            metadataOutput.setMetadataObjectsDelegate(context.coordinator, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean13, .ean8, .qr]
+        } else {
+            return viewController
+        }
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = viewController.view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        viewController.view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    
+    class Coordinator: NSObject, AVCaptureMetadataOutputObjectsDelegate {
+        var parent: BarcodeScannerView
+        
+        init(_ parent: BarcodeScannerView) {
+            self.parent = parent
+        }
+        
+        func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+            if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject, let barcodeValue = metadataObject.stringValue {
+                parent.onCaptureBarcode(barcodeValue)
+            }
+        }
+    }
+}
---
+++ VenueList/Details/VenueDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  VenueDetailViewModel.swift
+//  VenueList
+//
+//  Created by John Paul Manoza (NCS) on 2/11/25.
+//
+
+import Foundation
