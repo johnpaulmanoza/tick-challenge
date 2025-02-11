8800b1ef3e2bb697e7bd99d7042134d9f1b001d6
John Paul Manoza <johnpaul.manoza@ph.ncs-i.com>
2025-02-11 14:04:07 +0800

    feat(list): added model file for venue response

---
+++ VenueList/List/Venue.swift
@@ -0,0 +1,39 @@
+//
+//  Venue.swift
+//  VenueList
+//
+//  Created by John Paul Manoza (NCS) on 2/11/25.
+//
+
+import Foundation
+
+struct VenuesResponse: Codable {
+    let venues: [Venue]
+}
+
+struct Venue: Codable {
+    let code: String
+    let name: String
+    let address: String
+    let city: String
+    let state: String
+    let postcode: String
+    let latitude: Double
+    let longitude: Double
+    let timezone: String
+    let paxLocations: [PaxLocation]
+
+    enum CodingKeys: String, CodingKey {
+        case code, name, address, city, state, postcode, latitude, longitude, timezone
+        case paxLocations = "pax_locations"
+    }
+}
+
+struct PaxLocation: Codable {
+    let name: String
+    let gates: [Gate]
+}
+
+struct Gate: Codable {
+    let name: String
+}
